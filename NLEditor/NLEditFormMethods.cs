58550d961b415fedd5f8f9a7f963c61b93f45e97
Willicious <williciousmedia@gmail.com>
2026-01-18 18:53:20 +0000

    Bugfix - MainStyle combo is set correctly when refreshing styles

--- NLEditor/NLEditFormMethods.cs
+++ NLEditor/NLEditFormMethods.cs
@@ -697,7 +697,11 @@ Digger=20";
 
                 if (refreshedFromStyleManager)
                 {
-                    combo_MainStyle.SelectedIndex = 0;
+                    if ((CurLevel.MainStyle != null) && combo_MainStyle.Items.Contains(CurLevel.MainStyle.NameInEditor))
+                        combo_MainStyle.SelectedItem = CurLevel.MainStyle.NameInEditor;
+                    else
+                        combo_MainStyle.SelectedIndex = 0;
+
                     combo_PieceStyle.SelectedIndex = 0;
                 }
             }

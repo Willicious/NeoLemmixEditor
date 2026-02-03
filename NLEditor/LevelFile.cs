8f24e33cabf06b28cb216f5b5dd9079bd9459a5c
Willicious <williciousmedia@gmail.com>
2026-02-03 13:33:09 +0000

    Bugfix - Explicitly pass indent overload as 0
    + Previously, it was being passed as the index of the terrain piece, causing terrain piece indent explosion

--- NLEditor/LevelFile.cs
+++ NLEditor/LevelFile.cs
@@ -668,7 +668,7 @@ namespace NLEditor
 
             textFile.WriteLine("#        Terrain pieces         ");
             textFile.WriteLine("# ----------------------------- ");
-            curLevel.TerrainList.ForEach(ter => WriteTerrain(textFile, ter, curLevel.TerrainList.IndexOf(ter)));
+            curLevel.TerrainList.ForEach(ter => WriteTerrain(textFile, ter, curLevel.TerrainList.IndexOf(ter), 0));
             textFile.WriteLine(" ");
 
             if (curLevel.GadgetList.Exists(gad => gad.ObjType == C.OBJ.LEMMING))

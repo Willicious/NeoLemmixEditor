ca760816f887fb6f8b0edf956fe115940423d9d3
Willicious <williciousmedia@gmail.com>
2026-09-07 01:10:31 +0100

    Piece Search - Fit all items in the trigger effect combo (no scrolling)

--- NLEditor/FormPieceSearch.cs
+++ NLEditor/FormPieceSearch.cs
@@ -29,6 +29,7 @@ namespace NLEditor
             this.curStyle = curStyle;
 
             check_CurrentStyleOnly.Checked = false;
+            cbTriggerEffect.MaxDropDownItems = cbTriggerEffect.Items.Count; // Fit all items in the dropdown without scrolling
         }
 
         private void FilterSearchResults()

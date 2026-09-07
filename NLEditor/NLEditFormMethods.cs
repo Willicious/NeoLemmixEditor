1b39602a85cef1f285355d73ac927e535252f1f0
Willicious <williciousmedia@gmail.com>
2026-09-07 01:13:05 +0100

    Bugfix - Position metadata panel at the bottom of Pieces tab
    + Moved SetMetaDataPanel to UpdateNLEditorForm.cs

--- NLEditor/NLEditFormMethods.cs
+++ NLEditor/NLEditFormMethods.cs
@@ -2619,15 +2619,6 @@ Digger=20";
             }
         }
 
-        private void SetMetaDataPanel()
-        {
-            panelPieceMetaData.ForeColor = Color.SteelBlue;
-            btnLoadStyle.Top = tabPieces.Height - btnLoadStyle.Height;
-            panelPieceMetaData.Top = btnLoadStyle.Top - panelPieceMetaData.Height;
-            panelPieceMetaData.Left = tabPieces.Left;
-            panelPieceMetaData.Width = tabPieces.Width - 5;
-        }
-
         private void SetAllSkillsToZero()
         {
             foreach (Control ctrl in tabSkills.Controls)
